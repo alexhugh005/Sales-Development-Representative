d4fd451b898428d74b958de38464e4cb6d83c22a
Alex Shu <alexshu005@gmail.com>
2025-12-30 13:47:54 -0600

    Delete setting.py

--- setting.py
+++
@@ -1,4 +0,0 @@
-2025-12-30 12:34:25.225 [warning] [GitFileSystemProvider][stat] File not found - git:/c%3A/Users/FTGMSF22/Documents/Grok/Sales-Development-Representative/Sales-Development-Representative/.git/COMMIT_EDITMSG.git?%7B%22path%22%3A%22c%3A%5C%5CUsers%5C%5CFTGMSF22%5C%5CDocuments%5C%5CGrok%5C%5CSales-Development-Representative%5C%5CSales-Development-Representative%5C%5C.git%5C%5CCOMMIT_EDITMSG%22%2C%22ref%22%3A%22%22%7D
-2025-12-30 12:34:25.407 [info] > git blame --root --incremental cfcba42445e0ebce5df7aea0ff7e213020a8420b -- .git/COMMIT_EDITMSG [158ms]
-2025-12-30 12:34:25.407 [info] fatal: no such path .git/COMMIT_EDITMSG in cfcba42445e0ebce5df7aea0ff7e213020a8420b
-2025-12-30 12:34:25.522 [info] > git check-ignore -v -z --stdin [177ms]
